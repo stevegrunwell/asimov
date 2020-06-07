3288173b1acacabcc35fddd507c6306330943909
Steve Grunwell <steve@stevegrunwell.com>
2020-06-07 13:50:58 -0400

    Fix coding standards to comply with 'brew audit --new-formula'

--- Formula/asimov.rb
+++ Formula/asimov.rb
@@ -1,5 +1,5 @@
 class Asimov < Formula
-  desc "Automatically exclude development dependencies from Apple Time Machine backups "
+  desc "Automatically exclude development dependencies from Time Machine backups"
   homepage "https://github.com/stevegrunwell/asimov"
   url "https://github.com/stevegrunwell/asimov/archive/v0.2.0.tar.gz"
   sha256 "2efb456975af066a17f928787062522de06c14eb322b2d133a8bc3a764cc5376"
@@ -21,21 +21,22 @@ class Asimov < Formula
   plist_options :startup => true, :manual => "asimov"
 
   # Define the agent plist contents
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-        <dict>
-            <key>Label</key>
-            <string>#{plist_name}</string>
-            <key>Program</key>
-            <string>#{opt_bin}/asimov</string>
-            <key>StartInterval</key>
-            <!-- 24 hours = 60 * 60 * 24 -->
-            <integer>86400</integer>
-        </dict>
-    </plist>
-  EOS
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+          <dict>
+              <key>Label</key>
+              <string>#{plist_name}</string>
+              <key>Program</key>
+              <string>#{opt_bin}/asimov</string>
+              <key>StartInterval</key>
+              <!-- 24 hours = 60 * 60 * 24 -->
+              <integer>86400</integer>
+          </dict>
+      </plist>
+    EOS
   end
 
   # Define tests to run
